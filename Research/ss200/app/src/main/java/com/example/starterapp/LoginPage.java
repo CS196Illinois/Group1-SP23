3745302dec1c766e3518f551ad0af31bbdcfe7d0
soundaryaasivakumar <ss200@illinois.edu>
2023-04-20 17:44:42 -0500

    added Login Page

--- Research/ss200/app/src/main/java/com/example/starterapp/LoginPage.java
+++ Research/ss200/app/src/main/java/com/example/starterapp/LoginPage.java
@@ -1,6 +1,22 @@
 package com.example.starterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
+    public void createAccount(){
+        Button createNewAccountButton = new Button();
+        // create callback function so that when user wants to create an account, set the fields
+        createNewAccountButton.setId();
+
+    }
+    public void attemptLogin() {
+
+    }
 }
